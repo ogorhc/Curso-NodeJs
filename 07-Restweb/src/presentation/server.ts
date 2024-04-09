1086cb8b26999f84cc016d897f527bb88017936a
Tu nombre <i.hinojosa@ingreen.es>
2024-04-09 15:09:46 +0200

    Sección 14 -> fin de la sección, configuradas las rutas de los todos (CRUD), configuración de archivos de rutas

--- 07-Restweb/src/presentation/server.ts
+++ 07-Restweb/src/presentation/server.ts
@@ -1,30 +1,44 @@
-import express from "express";
+import express, { Router } from "express";
 import path from "path";
 
 interface ServerOptions {
   port: number;
   public_path: string;
+  routes: Router;
 }
 
 export class Server {
   private app = express();
   private readonly port: number;
   private readonly publicPath: string;
+  private readonly routes: Router;
 
   constructor(options: ServerOptions) {
-    const { port, public_path = "public" } = options;
+    const { port, public_path = "public", routes } = options;
     this.port = port;
     this.publicPath = public_path;
+    this.routes = routes;
   }
   async start() {
     /*
 		Middlewares
 	*/
+
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+
     /*
 		Public Folder
 	*/
     this.app.use(express.static(this.publicPath));
 
+    /* 
+	Routes
+	*/
+
+    this.app.use(this.routes);
+
+    /* SPA */
     this.app.get("*", (req, res) => {
       const indexPath = path.join(
         __dirname + `../../../${this.publicPath}/index.html`
