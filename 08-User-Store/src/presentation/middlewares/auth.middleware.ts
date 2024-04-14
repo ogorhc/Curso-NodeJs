f0d5f86a904103425b789a2d4cacdfdfd9a1215c
Tu nombre <i.hinojosa@ingreen.es>
2024-04-14 22:09:43 +0200

    Sección 20 -> Agregado authmiddleware

---
+++ 08-User-Store/src/presentation/middlewares/auth.middleware.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import { JwtAdapter } from "../../config";
+import { UserModel } from "../../data";
+import { UserEntity } from "../../domain";
+
+export class AuthMiddleware {
+  static async validateJWT(req: Request, res: Response, next: NextFunction) {
+    const authorization = req.header("Authorization");
+
+    if (!authorization)
+      return res.status(401).json({ error: "No token provided" });
+
+    if (!authorization.startsWith("Bearer "))
+      return res.status(401).json({ error: "Invalid Bearer Token" });
+
+    const token = authorization.split(" ").at(1) || "";
+
+    try {
+      const payload = await JwtAdapter.validateToken<{ id: string }>(token);
+      if (!payload) return res.status(401).json({ error: "Invalid token" });
+
+      const user = await UserModel.findById(payload.id);
+
+      if (!user) return res.status(401).json({ error: "Invalid Token - user" });
+
+      req.body.user = UserEntity.fromObject(user);
+
+      next();
+
+      //todo: validar si el usuario está activo
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+}
