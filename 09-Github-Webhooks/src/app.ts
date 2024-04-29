817e99daf57ffabedd5873f0a40104c863988bbc
Tu nombre <i.hinojosa@ingreen.es>
2024-04-29 09:03:19 +0200

    Sección 23 -> Webhook desde github y envío a Discord mediante Bot

---
+++ 09-Github-Webhooks/src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { envs } from "./config";
+import { GithubController } from "./presentation/github/controller";
+
+(() => {
+  main();
+})();
+
+function main() {
+  const app = express();
+
+  app.use(express.json());
+
+  const controller = new GithubController();
+
+  app.post("/api/github", controller.webhookHandler);
+
+  app.listen(envs.PORT, () => {
+    console.log(`App running on port ${envs.PORT}`);
+  });
+}
